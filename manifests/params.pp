22e36a6b181b166346d5b78726fecb7e44b72579
James Fryman <james@frymanet.com>
2012-02-19 19:59:27 -0600

    removing unncessary package

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class mysql::params {
     ubuntu,debian: {
       $ml_packages = [ 'libdbd-mysql-perl', 'libdbi-perl', 'libhtml-template-perl',
                        'libmysqlclient16', 'libnet-daemon-perl', 'libplrpc-perl',
-                       'mysql-client-5.1', 'mysql-client-core-5.1', 'mysql-common',
+                       'mysql-client-5.1', 'mysql-common',
                        'mysql-server', 'mysql-server-5.1', 'mysql-server-core-5.1',
         ]
       $ml_mysql_conf = '/etc/mysql/my.cnf'
